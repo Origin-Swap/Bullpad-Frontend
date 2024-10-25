da053292bf5ae2748ae9ab6c08833c368a5e0932
origin-swap <ah0229247@gmail.com>
2024-10-26 00:27:43 +0700

    Fix import svg

--- apps/web/src/views/Account/svgs/index.tsx
+++ apps/web/src/views/Account/svgs/index.tsx
@@ -1,5 +1,5 @@
-export { default as WebIcon } from './Web';
-export { default as TeleIcon } from './Telegram';
-export { default as TwitterIcon } from './Twitter';
-export { default as DiscordIcon } from './Discord';
-export { default as InstaIcon } from './Insta';
+export { default as WebIcon } from './Web.tsx';
+export { default as TeleIcon } from './Telegram.tsx';
+export { default as TwitterIcon } from './Twitter.tsx';
+export { default as DiscordIcon } from './Discord.tsx';
+export { default as InstaIcon } from './Insta.tsx';
